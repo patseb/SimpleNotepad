6ebd968c62d9e657fe45c52dd8bd8811a557e87d
Michał Kazior (Kazik) <kazikcz@gmail.com>
2011-06-24 22:55:30 +0200

    rename activity
    Oops, I forgot about this..
    
    Signed-off-by: Patryk Kwiatkowski <patseb@gmail.com>

--- src/com/patrykkwiatkowski/simplenotepad/NoteList.java
+++ src/com/patrykkwiatkowski/simplenotepad/NoteList.java
@@ -40,7 +40,7 @@ public class NoteList extends Activity {
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.main);
+		setContentView(R.layout.notelist);
 
 		AC = (ApplicationController)getApplicationContext();
 		notes = AC.getNotes();
