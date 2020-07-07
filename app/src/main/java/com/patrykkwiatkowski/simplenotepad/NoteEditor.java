bcb8ffda115bb04a0b23a3927def9af955a8674d
Patryk Kwiatkowski <patseb@gmail.com>
2020-07-07 15:34:12 +0200

    Switch to gardle and "All Notes" button
    As Android 10.0 removed menu button emulation we have to create way to access to all notes

---
+++ app/src/main/java/com/patrykkwiatkowski/simplenotepad/NoteEditor.java
@@ -0,0 +1,92 @@
+package com.patrykkwiatkowski.simplenotepad;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+/**
+ * Note editor activity.
+ * 
+ * The user may edit or create text content for note.
+ */
+public class NoteEditor extends Activity {
+	private EditText editText;
+	private Button save;
+	private Button all;
+	private Note note;
+
+	private void save() {
+		String text = editText.getText().toString();
+		if (text.length() > 0) {
+			if (note == null) note = new Note();
+			if (note.getTextContent() == text) return;
+			note.setTextContent(text);
+			NoteStorage.save(note, new Runnable() {
+				@Override
+				public void run() {
+					NoteEditor.this.runOnUiThread(new Runnable() {
+						@Override
+						public void run() {
+							Toast.makeText(NoteEditor.this, R.string.err_creation,
+									Toast.LENGTH_SHORT).show();
+						}
+					});
+				}
+			});
+		}
+	}
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.editnote);
+
+		Bundle request = this.getIntent().getExtras();
+		if (request != null && request.containsKey("note")) {
+			note = (Note) request.get("note");
+		}
+
+		editText = (EditText) findViewById(R.id.editNoteContentEditText);
+		if (note != null) editText.setText(note.getTextContent());
+
+		save = (Button) findViewById(R.id.editNoteSaveButton);
+		save.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				save();
+				editText.setText("");
+				note = null;
+			}
+		});
+
+		all = (Button) findViewById(R.id.editNoteAllButton);
+		all.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				startActivity(new Intent(NoteEditor.this, NoteList.class));
+			}
+		});
+
+	}
+
+	@Override
+	public boolean onKeyDown(int keyCode, KeyEvent event) {
+		if (keyCode == KeyEvent.KEYCODE_MENU) {
+			startActivity(new Intent(this, NoteList.class));
+			return true;
+		}
+		return super.onKeyDown(keyCode, event);
+	}
+
+	@Override
+	public void onBackPressed() {
+		save();
+		super.onBackPressed();
+	}
+}
